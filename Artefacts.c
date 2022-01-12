0b19f217857d54f5bf5d74f92a3501d58b100617
meglin97 <78850286+meglin97@users.noreply.github.com>
2022-01-12 11:52:29 +0100

    Update Artefacts.c

--- Artefacts.c
+++ Artefacts.c
@@ -1,35 +1,5 @@
-
-#include <stdbool.h>
 #include <CUnit.h>
-
-enum Action
-{
-    FREINAGE_URGENT,
-    FREINER,
-    AVANCER
-
-};
-typedef struct SysFrein
-{
-    char frein[10];
-    enum Action order;
-} SysFrein;
-
-typedef struct MSG
-{
-    char msg[20];
-    int shift;
-} MSG;
-
-typedef struct CSC
-{
-    char msg_detect_in[20];
-    char msg_frein_in[20];
-    char msg_danger_in[20];
-    char msg_danger_out[20];
-    char order_out[20];
-    enum Action order;
-} CSC;
+#include <Artefacts.h>
 
 void encrypt(char str[], char str_out[])
 {
@@ -86,7 +56,6 @@ void decrypt(char str[], char str_out[], int shift)
     }
 }
 
-
 void CSC_control(char danger_info[20], char frein_info[20], char msg_danger_in[20], SysFrein sf, CSC csc)
 {
     if (strcmp(danger_info, "danger") || strcmp(msg_danger_in, "danger"))
@@ -102,6 +71,7 @@ void CSC_control(char danger_info[20], char frein_info[20], char msg_danger_in[2
         sf.order = AVANCER;
     }
 }
+
 void CU(CSC csc, MSG msg_danger_out, MSG msg_danger_in)
 {
     msg_danger_out.shift = 5;
