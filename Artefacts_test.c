856536e53f9013ae755e950b97eb38777d39e687
JorisJC <33754664+Ti-joko@users.noreply.github.com>
2022-01-13 11:33:05 +0100

    Add files via upload

---
+++ Artefacts_test.c
@@ -0,0 +1,39 @@
+#include <CUnit/CUnit.h>
+
+#include "Artefacts.h"
+
+int init_suite(void) { return 0;}
+int clean_suite(void) { return 0;}
+
+
+
+void encrypt_test_1(void) {
+	CU_ASSERT_EQUAL(encrypt("test","test"), "yjxy");
+}
+
+int main(void) {
+
+CU_pSuite pSuite = NULL;
+
+if(CUE_SUCCESS != CU_initialize_registry())
+	return CU_get_error();
+
+	pSuite = CU_add_suite("encrypt_test_suite", init_suite, clean_suite);
+	if(NULL == pSuite) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if((NULL == CU_add_test(pSuite, "encrypt_test_1")))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	printf("\n");
+	CU_automated_run_tests();
+	CU_cleanup_registry();
+	return CU_get_error();
+}
